bac5907f3086c10a9e479fa0095b1e9acdae5e30
cristianmino <cristianeisasa@gmail.com>
2023-01-21 21:14:44 -0300

    Resolviendo el problema del ejercicio

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ class App extends Component {
       list: ['Javascript', 'CSS', 'HTLM', 'React']
     }
   }
+
   render() {
     return (
       <div style={styles.container}>
         <h1 style={styles.title}>Las tecnologias del Front</h1>
         <ul style={styles.list}>
-          {/* Tu codigo va aca */}
+          {this.state.list.map((item) => <li>{item}</li>)}
         </ul>
       </div>
     );
